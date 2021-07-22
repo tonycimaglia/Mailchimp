e0e5842c484b5839e90568fa24fe85fae3b99aed
Tony Cimaglia <tcimaglia@pulte.com>
2021-07-22 16:53:11 -0400

    Moved converters to their own directory. Created Converter engine and tests. Added more test files, and targeted the tesfile directory to be copied to project output.

---
+++ Markdown.Converter.Tests/ConverterEngineTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+using System.Text;
+
+namespace Markdown.Converter.Tests
+{
+    public class ConverterEngineTest
+    {
+        private ConverterEngine _converterEngine;
+
+        [SetUp]
+        public void Setup()
+        {
+            _converterEngine = new ConverterEngine();
+        }
+
+        [TestCase("InputTest.md", "ActualTest.html")]
+        [TestCase("InputTest2.md", "ActualTest2.html")]
+        public void ValidateConversion(string markdownFile, string htmlFile)
+        {
+            var directoryPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var markDown = System.IO.File.ReadAllLines($"{directoryPath}\\TestFiles\\{markdownFile}");
+            var expectedHtml = System.IO.File.ReadAllLines($"{directoryPath}\\TestFiles\\{htmlFile}");
+
+            var convertedHtml = _converterEngine.ConvertToHtml(markDown);
+
+            Assert.That(convertedHtml, Is.EquivalentTo(expectedHtml));
+        }
+    }
+}
---
+++ Markdown.Converter/Converters/ConverterEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown.Converter
+{
+    public class ConverterEngine
+    {
+        public IEnumerable<string> ConvertToHtml(IEnumerable<string> markdown)
+        {
+            // loop and determine what kind of conversion should happen for each string
+            // combine sequential lines of unformatted text
+            // use converter factory to instantiate correct converter
+            // convert and add to html string array
+            return new List<string>();
+        }
+    }
+}
