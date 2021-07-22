db8dbab5fd8a228cc638ff3fb9d48f2ecd8ff62f
Tony Cimaglia <tcimaglia@pulte.com>
2021-07-22 14:44:34 -0400

    Wrote logic for link converter. Created textBetween method and wrote unit tests.

--- Markdown.Converter.Tests/LinkConverterTest.cs
+++ Markdown.Converter.Tests/LinkConverterTest.cs
@@ -12,12 +12,20 @@ namespace Markdown.Converter.Tests
             _linkConverter = new LinkConverter();
         }
 
-        [TestCase("[Link text](https://www.example.com)", @"<a href=""https://example.com"">Link text</a>")]
-        [TestCase("[Another Test Link](https://www.test.com)", @"<a href=""https://test.com"">Another Test Link</a>")]
+        [TestCase("[Link text](https://www.example.com)", @"<a href=""https://www.example.com"">Link text</a>")]
+        [TestCase("[Another Test Link](https://www.test.com)", @"<a href=""https://www.test.com"">Another Test Link</a>")]
         public void ValidateConversion(string markdown, string html)
         {
             var convertedHtml = _linkConverter.Convert(markdown);
             Assert.That(convertedHtml, Is.EqualTo(html));
+        }
+
+        [TestCase("[Link text](https://www.example.com)", "(", ")", "https://www.example.com")]
+        [TestCase("[Link text](https://www.example.com)", "[", "]", "Link text")]
+        public void ValidateFindTextBetween(string markdown, string char1, string char2, string actual)
+        {
+            var textBetween = _linkConverter.FindTextBetween(markdown, char1, char2);
+            Assert.That(textBetween, Is.EqualTo(actual));
         }
     }
 }
--- Markdown.Converter/Converters/LinkConverter.cs
+++ Markdown.Converter/Converters/LinkConverter.cs
@@ -1,11 +1,26 @@
 ﻿
+using System.Text;
+
 namespace Markdown.Converter
 {
-    public class LinkConverter
+    public class LinkConverter : IConverter
     {
         public string Convert(string markdown)
         {
-            return "";
+            string linkText = FindTextBetween(markdown, "[", "]");
+            string url = FindTextBetween(markdown, "(", ")");
+            var htmlHref = $"<a href=\"{url}\">{linkText}</a>";
+            return htmlHref;
+        }
+
+        public string FindTextBetween (string markdownLink, string char1, string char2)
+        {
+            int startingPosition = markdownLink.IndexOf(char1) + char1.Length;
+            int endingPostion = markdownLink.LastIndexOf(char2);
+            int lengthOfTextBetween = endingPostion - startingPosition;
+
+            string textBetween = markdownLink.Substring(startingPosition, lengthOfTextBetween);
+            return textBetween;
         }
     }
 }
