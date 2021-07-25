d95fe37c4d4d407c8566addb25939e22cf2cbc5e
Tony Cimaglia <tcimaglia@pulte.com>
2021-07-25 18:55:30 -0400

    added simple logic to the Main method to print any input located in the SampleInput directory.

--- Markdown.Converter/Program.cs
+++ Markdown.Converter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Markdown.Converter
 {
@@ -6,6 +7,18 @@ namespace Markdown.Converter
     {
         static void Main(string[] args)
         {
+            var directoryPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string[] files = System.IO.Directory.GetFiles($"{directoryPath}\\SampleInputs", "*.md");
+            foreach (var file in files)
+            {
+                var converter = new ConverterEngine();
+                var markDown = System.IO.File.ReadAllLines(file);
+                var html = converter.ConvertToHtml(markDown);
+                foreach(var line in html)
+                {
+                    Console.WriteLine($"{line}\n");
+                }
+            }
             Console.WriteLine("Hello World!");
         }
     }
